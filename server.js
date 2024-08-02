94488411a70e5ba18475b98854c5cc208ec799db
basit-devBE <basitmohammed362@gmail.com>
2024-08-02 14:55:01 +0000

    hi

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 import express from 'express'
 import { configDotenv } from 'dotenv'
+import dbConfig from './config/dbConfig.js'
 configDotenv()
 
 const app = express()
 const PORT = process.env.PORT || 5000
+
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+dbConfig();
 
 app.listen(PORT , () => {
     console.log(`Server is running on port ${PORT}`)
